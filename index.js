a34d91d00c3fde05292a1467708096c94753a15a
PalashNandanwar <arunpalash1@gmail.com>
2025-03-05 12:25:51 +0530

    Backend

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server"); // Import json-server
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; // Choose a port (8080, 3001, etc.)
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser); // Enable JSON body parsing
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on http://localhost:${port}`);
+});
